5ab1be19061d8b03569c48472876e6958ce77752
alfanugraha <alfa.nugraha4@gmail.com>
2016-09-20 16:47:43 +0700

    update: how to use data and helpers

--- app/shiny/app.R
+++ app/shiny/app.R
@@ -1,15 +1,58 @@
-# header
+source('helpers.R')
+counties <- readRDS('data/counties.rds')
+
+
+#=============================== DASHBOARD HEADER ====================================
+#
+#   Create a header for a dashboard page
+#   It can be left blank, or it can include dropdown menu items on the right side
+#   
+#       dashboardHeader(..., title=NULL, titleWidth = NULL, disable=FALSE, .list=NULL)
+#
+#       ...     Items to put in the header. Should be drowdownMenu(s)
+#       .list   An optional list containing items to put in the header
+#
+#======================================================================================
 header <- dashboardHeader(title='LUMENS Dashboard')
 
-# sidebar
+#=============================== DASHBOARD SIDEBAR ====================================
+#
+#   Create a dashboard sidebar
+#   It typically contains a sidebarMenu, sidebarSearchForm, or other Shiny inputs
+#   
+#       dashboardSidebar(..., disable=FALSE, width=NULL)
+#
+#       ...     Items to put in the sidebar
+#       
+#======================================================================================
 sidebar <- dashboardSidebar(
     sidebarMenu(
         menuItem('Dashboard', tabName = 'dashboard', icon = icon('dashboard')),
-        menuItem('GDP', tabName = 'GDPWidgets', icon = icon('th'))
+        menuItem('GDP', tabName = 'GDPWidgets', icon = icon('th')),
+
+        selectInput("var", 
+            label = "Choose a variable to display",
+            choices = c("Percent White", "Percent Black",
+                        "Percent Hispanic", "Percent Asian"),
+            selected = "Percent White"),
+        
+        sliderInput("range", 
+            label = "Range of interest:",
+            min = 0, max = 100, value = c(0, 100))
+
     )
 )
 
-# body
+#=============================== DASHBOARD BODY =======================================
+#
+#   The main body of a dashboard page
+#   It typically contains a sidebarMenu, sidebarSearchForm, or other Shiny inputs
+#   
+#       dashboardSidebar(...)
+#
+#       ...     Items to put in the dashboard body
+#       
+#======================================================================================
 body <- dashboardBody(
     tags$head(
         tags$link(ref='stylesheet', type='text/css', href='style.css')
@@ -31,28 +74,11 @@ body <- dashboardBody(
         ),
         
         tabItem(tabName='GDPWidgets',
-                h2('GDP tab content'),
-                box(
-                    title='Penambahan Luasan (%)',
-                    status='primary',
-                    collapsible=TRUE,
-                    sliderInput('sliderUndisturbedForest', 'Undisturbed Forest', 0, 100, 0),
-                    sliderInput('sliderOilPalmMonoculture', 'Oil Palm Monoculture', 0, 100, 0),
-                    sliderInput('sliderSettlement', 'Settlement', 0, 100, 0),
-                    sliderInput('sliderWaterbody', 'Waterbody', 0, 100, 0)
-                ),
-                box(
-                    title=HTML('&#916;GDP (%)'),
-                    status='info',
-                    collapsible=TRUE,
-                    textOutput('textOutputPrecentageDeltaGDP')
-                ),
-                box(
-                    title=HTML('&#916;GDP (Million Rupiahs)'),
-                    status='info',
-                    collapsible=TRUE,
-                    textOutput('textOutputMillionDeltaGDP')
-                )
+            box(
+                title='Population Density',
+                collapsible=TRUE,
+                plotOutput('map')
+            )
         )
     )
 )
@@ -67,18 +93,57 @@ ui <- dashboardPage(
     body
 )
 
+#======================================== OUTPUT & RENDER FUNCTION ======================================
+#
+#       *Output function    creates             render* function    creates 
+#       ----------------------------            ---------------------------
+#       htmlOutput          raw HTML            renderImage         images (saved as a link to a source file)
+#       imageOutput         image               renderPlot          plots    
+#       plotOutput          plot                renderPrint         any printed output
+#       tableOutput         table               renderTable         data frame, matrix, other table like structures
+#       textOutput          text                renderText          character strings
+#       uiOutput            raw HTML            renderUI            a Shiny tag object or HTML
+#       verbatimTextOtput   text
+#   
+#=========================================================================================================
 #setup ui server
 server <- function(input, output, session) {
-    output$textOutputPrecentageDeltaGDP <- renderText({
-        mean(c(input$sliderUndisturbedForest, input$sliderOilPalmMonoculture, input$sliderSettlement, input$sliderWaterbody))
-    })
     
-    output$textOutputMillionDeltaGDP <- renderText({
-        paste(
-            sum(input$sliderUndisturbedForest, input$sliderOilPalmMonoculture, input$sliderSettlement, input$sliderWaterbody)*100
+    
+    # args <- switch(input$var,
+    #               "Percent White" = list(counties$white, "darkgreen", "% White"),
+    #               "Percent Black" = list(counties$black, "black", "% Black"),
+    #               "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+    #               "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+    
+    # args$min <- input$range[1]
+    # args$max <- input$range[2]
+    
+    # do.call(percent_map, args)
+    
+    output$map <- renderPlot({
+        data <- switch (input$var,
+            "Percent White" = counties$white,
+            "Percent Black" = counties$black,
+            "Percent Hispanic" = counties$hispanic,
+            "Percent Asian" = counties$asian
         )
+        color <- switch(input$var, 
+            "Percent White" = "darkgreen",
+            "Percent Black" = "black",
+            "Percent Hispanic" = "darkorange",
+            "Percent Asian" = "darkviolet"
+        )
+        legend <- switch(input$var, 
+             "Percent White" = "% White",
+             "Percent Black" = "% Black",
+             "Percent Hispanic" = "% Hispanic",
+             "Percent Asian" = "% Asian"
+        )
+        percent_map(var = data, color = color, legend.title = legend, max = input$range[2], min = input$range[1])
     })
     
+    # comment this function if on live reloading mode
     session$onSessionEnded(function(){
       stopApp()
     })
